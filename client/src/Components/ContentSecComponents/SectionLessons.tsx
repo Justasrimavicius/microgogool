f3dd813ee5273821096e0128bca6071d90abec42
Justinas <justasrimavicius@gmail.com>
2022-10-20 18:34:02 +0300

    Lesson page - able to select answers(doesnt allow selection of more values than initialized), able to go back(doesnt yet let go back to the same lesson twice in a row - will fix it). Animations are clunky, needs fixing.

--- client/src/Components/ContentSecComponents/SectionLessons.tsx
+++ client/src/Components/ContentSecComponents/SectionLessons.tsx
@@ -10,6 +10,9 @@ interface props{
             title?: string
         }
     }
+    goBack: {
+        loadSpecificSection: React.Dispatch<React.SetStateAction<boolean>>
+    }
 }
 interface questionInfoObj{
     correctAnswer: string[],
@@ -20,23 +23,26 @@ interface questionInfoObj{
 
 function SectionLessons(props: props) {
 
-    const mainCompRef = useRef<HTMLDivElement>(null);
+    const lessonsDivRef = useRef<HTMLDivElement>(null);
 
     const [arrayIndividualLessons, setArrayIndividualLessons] = useState<any>(Object.entries(props.specificSectionsData.individualLessons));
 
     useEffect(()=>{
         setTimeout(() => {
-            if(mainCompRef.current!=null){
-                mainCompRef.current.classList.add('lessons-div');
+            if(lessonsDivRef.current!=null){
+                lessonsDivRef.current.classList.add('lessons-div');
                 setTimeout(() => {
-                    if(mainCompRef.current!=null){
-                        mainCompRef.current.classList.remove('lessons-div-fadeInAnim');
+                    if(lessonsDivRef.current!=null){
+                        lessonsDivRef.current.classList.remove('lessons-div-fadeInAnim');
                     }
                 }, 2000);
             }   
         }, 100);
+
+
     },[]);
 
+    let lessonNumber = 0;
     const questionCounterRef = useRef<number>(0);
     function loadQuestions(arrayIndividualLessons: any): React.ReactElement | null{
 
@@ -50,12 +56,12 @@ function SectionLessons(props: props) {
                 // the element (arrayIndividualLessons[questionsCounterRef.current][1]) is question element - its second array elements contains info about questions
                 console.log('this array contains information about the question')
                 console.log(arrayIndividualLessons[questionCounterRef.current][1])
-
+                lessonNumber = arrayIndividualLessons[questionCounterRef.current][0].slice(6,7);
                 if(arrayIndividualLessons[questionCounterRef.current][1].questionFormat=='SelectOne'){
-                    return loadQuestion_SelectOne(arrayIndividualLessons[questionCounterRef.current][1]);
+                    return LoadQuestion_SelectOne(arrayIndividualLessons[questionCounterRef.current][1]);
                 }
                 if(arrayIndividualLessons[questionCounterRef.current][1].questionFormat=='SelectMultiple'){
-                    return loadQuestion_SelectMultiple(arrayIndividualLessons[questionCounterRef.current][1]);
+                    return LoadQuestion_SelectMultiple(arrayIndividualLessons[questionCounterRef.current][1]);
                 }
                 if(arrayIndividualLessons[questionCounterRef.current][1].questionFormat=='DragAndDrop'){
                     return loadQuestion_DragAndDrop(arrayIndividualLessons[questionCounterRef.current][1]);
@@ -84,19 +90,57 @@ function SectionLessons(props: props) {
         )
     }
 
-    function loadQuestion_SelectOne(questionInfoObj: questionInfoObj){
+    function LoadQuestion_SelectOne(questionInfoObj: questionInfoObj){
+        questionCounterRef.current++;
+
+        const answersSelected = useRef<HTMLElement[]>([]);
+
+        const possibleAnswersNum = questionInfoObj.correctAnswer.length;
+
+
+        function answer_SO_Selected(id: string){
+        // document.getElementById(`${index}SM-${singleAnswer}`)?.classList.add('singlePossibleAnswer-selected')
+
+            // clicked element
+            const selectedElement = document.getElementById(id);
+
+            if(selectedElement==null){
+                return;
+            }
+            // if user clicks the same answer, remove if from selected answers array and remove its answered class
+            if(answersSelected.current.includes(selectedElement)){
+                answersSelected.current.map((element,index)=>{
+                    if(element==selectedElement){
+                        answersSelected.current.splice(index,1);
+                        selectedElement.classList.remove('singlePossibleAnswer-selected');
+                    }
+                })
+                return;
+            }
+            // else, push the new clicked value to the array and add the class of selected answer
+            answersSelected.current.push(selectedElement);
+            selectedElement.classList.add('singlePossibleAnswer-selected');
+
+            // if more than max answers are selected(elements pushed in array), removed the first added element from array and remove its class
+            if(answersSelected.current.length>possibleAnswersNum){
+                const removedElement = answersSelected.current.shift();
+                if(removedElement==null)return;
+                removedElement.classList.remove('singlePossibleAnswer-selected');
+            }
+        }
 
         questionCounterRef.current++;
         return(
             <React.Fragment>
             <div className='question-SelectOne single-question'>
-                <p className='single-question-title'>{questionInfoObj.title}</p>
+                <p className='single-question-title'>Question {lessonNumber}: {questionInfoObj.title}<br></br><em>{questionInfoObj.correctAnswer.length} possible answer/s</em></p>
                 <div className='single-question-possibleAnswers'>{questionInfoObj.possibleAnswers.map((singleAnswer,index)=>{
                     return(
                         <button className='single-question-possibleAnswers-singleAnswer' 
                         key={`${index}SO-${singleAnswer}`}
                         id={`${index}SO-${singleAnswer}`}
-                        onClick={()=>{document.getElementById(`${index}SO-${singleAnswer}`)?.classList.add('singlePossibleAnswer-selected')}}
+                        // onClick={()=>{document.getElementById(`${index}SO-${singleAnswer}`)?.classList.add('singlePossibleAnswer-selected')}}
+                        onClick={()=>{answer_SO_Selected(`${index}SO-${singleAnswer}`)}}
                         >
                         {singleAnswer}</button>
                     )
@@ -108,20 +152,55 @@ function SectionLessons(props: props) {
         )
 
     }
-    function loadQuestion_SelectMultiple(questionInfoObj: questionInfoObj){
-
+    function LoadQuestion_SelectMultiple(questionInfoObj: questionInfoObj){
         questionCounterRef.current++;
+
+        const answersSelected = useRef<HTMLElement[]>([]);
+
+        const possibleAnswersNum = questionInfoObj.correctAnswer.length;
+
+
+        function answer_SM_Selected(id: string){
+        // document.getElementById(`${index}SM-${singleAnswer}`)?.classList.add('singlePossibleAnswer-selected')
+
+            // clicked element
+            const selectedElement = document.getElementById(id);
+
+            if(selectedElement==null){
+                return;
+            }
+            // if user clicks the same answer, remove if from selected answers array and remove its answered class
+            if(answersSelected.current.includes(selectedElement)){
+                answersSelected.current.map((element,index)=>{
+                    if(element==selectedElement){
+                        answersSelected.current.splice(index,1);
+                        selectedElement.classList.remove('singlePossibleAnswer-selected');
+                    }
+                })
+                return;
+            }
+            // else, push the new clicked value to the array and add the class of selected answer
+            answersSelected.current.push(selectedElement);
+            selectedElement.classList.add('singlePossibleAnswer-selected');
+
+            // if more than max answers are selected(elements pushed in array), removed the first added element from array and remove its class
+            if(answersSelected.current.length>possibleAnswersNum){
+                const removedElement = answersSelected.current.shift();
+                if(removedElement==null)return;
+                removedElement.classList.remove('singlePossibleAnswer-selected');
+            }
+        }
 
         return(
             <React.Fragment>
                 <div className='question-SelectMultiple single-question'>
-                    <p className='single-question-title'>{questionInfoObj.title}</p>
+                    <p className='single-question-title'>Question {lessonNumber}: {questionInfoObj.title}<br></br><em>{questionInfoObj.correctAnswer.length} possible answer/s</em></p>
                     <div className='single-question-possibleAnswers'>{questionInfoObj.possibleAnswers.map((singleAnswer,index)=>{
                         return(
                             <button className='single-question-possibleAnswers-singleAnswer' 
                             key={`${index}SM-${singleAnswer}`}
                             id={`${index}SM-${singleAnswer}`}
-                            onClick={()=>{document.getElementById(`${index}SM-${singleAnswer}`)?.classList.add('singlePossibleAnswer-selected')}}
+                            onClick={()=>{answer_SM_Selected(`${index}SM-${singleAnswer}`)}}
                             >
                             {singleAnswer}</button>
                         )
@@ -138,7 +217,7 @@ function SectionLessons(props: props) {
         return(
             <React.Fragment>
                 <div className='question-DragAndDrop single-question'>
-                    <p className='single-question-title'>{questionInfoObj.title}</p>
+                    <p className='single-question-title'>Question {lessonNumber}: {questionInfoObj.title}</p>
                     <div className='single-question-DragAndDrop-words'>{questionInfoObj.possibleAnswers.map((singleWord,index)=>{
                         return(
                             <button className='single-question-DragAndDrop-singleWord'
@@ -157,12 +236,25 @@ function SectionLessons(props: props) {
     }
 
 
+    function returnToMain(){
+        const mainPath = document.querySelector('.main-path') as HTMLElement;
+        console.log(lessonsDivRef.current);
+        lessonsDivRef.current?.classList.add('lessons-div-fadeOutAnim');
+        lessonsDivRef.current?.classList.remove('lessons-div');
+        setTimeout(() => {
+            props.goBack.loadSpecificSection(false);
+        }, 1000);
+    }
 
 
     return (
-        <div className='lessons-div-fadeInAnim' ref={mainCompRef}>
+        <div className='lessons-div-fadeInAnim' ref={lessonsDivRef}>
             <div className='single-lesson'>
                 {loadQuestions(arrayIndividualLessons)}
+            <div className='single-lesson-buttons'>
+                <button className='lesson-answer-submit'>Submit the answers</button>
+                <button className='lesson-go-back' onClick={()=>{returnToMain()}}>Go back</button>
+            </div>
             </div>
         </div>
     );
--- client/src/Components/MainSections/Content.tsx
+++ client/src/Components/MainSections/Content.tsx
@@ -19,11 +19,20 @@ function Content() {
     const mainPathRef = useRef<HTMLDivElement>(null);
     useEffect(()=>{
         if(sectionNum!=-1){
-            setTimeout(() => {
-                if(mainPathRef.current!=null){ // checks for typescript
-                    mainPathRef.current.style.display='none';
-                }
-            }, 2000);
+            if(mainPathRef.current!=null){ // checks for typescript
+                mainPathRef.current.style.opacity='0';
+                setTimeout(() => {
+                    if(mainPathRef.current!=null){
+                        mainPathRef.current.style.display='none';
+                        const footer = document.querySelector('footer');
+                        if(footer){
+                            footer.style.opacity='0';
+                            footer.style.visibility='hidden';
+                        }
+                        loadSpecificSection(true);
+                    }
+                }, 500);
+            }
 
             if(allSectionsData!=null){
                 allSectionsData.map(specificSection=>{
@@ -32,7 +41,6 @@ function Content() {
                     }
                 })
             }
-            loadSpecificSection(true);
         }
 
     },[sectionNum])
@@ -46,13 +54,27 @@ function Content() {
             })
     },[])
 
+    useEffect(()=>{
+        if(specificSection!=true){
+            if(mainPathRef.current!=null){
+                mainPathRef.current.style.opacity='1';
+                mainPathRef.current.style.display='block';
+                const footer = document.querySelector('footer');
+                if(footer){
+                    footer.style.opacity='1';
+                    footer.style.visibility='visible';
+                }
+            }
+        }
+    },[specificSection])
+
     return (
         <main>
             <nav className='main-left-nav'>
 
             </nav>
             {allSectionsData.length!= 1 ? <MainPath sectionLessons={{sectionNum, setSectionNum}} refs={{mainPathRef}} allSectionsDataState={{allSectionsData, setAllSectionsData}}/> : null}
-            {specificSection!=false ? <SectionLessons sectionNum={sectionNum} specificSectionsData={specificSectionsData}/> : null}
+            {specificSection!=false ? <SectionLessons sectionNum={sectionNum} specificSectionsData={specificSectionsData} goBack={{loadSpecificSection}}/> : null}
             <div className='main-right'>
 
             </div>
